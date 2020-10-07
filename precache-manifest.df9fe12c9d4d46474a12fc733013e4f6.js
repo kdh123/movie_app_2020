90effa6b2784f0be7496340640326e354c30e600
kdh123 <dnlwkem39@gmail.com>
2020-10-07 10:19:32 +0900

    Updates

---
+++ precache-manifest.df9fe12c9d4d46474a12fc733013e4f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65bcdd54daaa58ddd644552cbf29579f",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "5832dca8febdc57a70f3",
+    "url": "/movie_app_2020/static/css/main.c698557a.chunk.css"
+  },
+  {
+    "revision": "a7d22b2c00d8d326c863",
+    "url": "/movie_app_2020/static/js/2.300fc6d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.300fc6d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5832dca8febdc57a70f3",
+    "url": "/movie_app_2020/static/js/main.47cc4bf1.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
